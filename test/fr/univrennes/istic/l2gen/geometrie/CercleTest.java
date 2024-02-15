64ce01defe5289e6975e035ff2e305d140d4c147
Juliette Karman-Besson <juliette.karman-besson@etudiant.univ-rennes.fr>
2024-02-15 19:10:25 +0100

    début des tests de Cercle

--- test/fr/univrennes/istic/l2gen/geometrie/CercleTest.java
+++ test/fr/univrennes/istic/l2gen/geometrie/CercleTest.java
@@ -1,31 +1,50 @@
 package fr.univrennes.istic.l2gen.geometrie;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CercleTest {
-    @Test
-    public void testCentre() {
+    private Cercle cercleTest;
+    private Point pointCerc;
+    private double rayonCerc;
 
+    @Before
+    public void initCerc() {
+        double rayonCerc = 25;
+        Point pointCerc = new Point(50, 40);
+        Cercle cercleTest = new Cercle(pointCerc, rayonCerc);
+        this.cercleTest = cercleTest;
+        this.pointCerc = pointCerc;
+        this.rayonCerc = rayonCerc;
     }
 
     @Test
-    public void testColorier() {
-
+    public void testCentre() {
+        assertEquals(pointCerc, cercleTest.centre());
     }
 
     @Test
     public void testDeplacer() {
-
+        double x = 5;
+        double y = 10;
+        cercleTest.deplacer(x, y);
+        assertEquals(55, pointCerc.x(), 0.0001);
+        assertEquals(50, pointCerc.y(), 0.0001);
     }
 
     @Test
     public void testDescription() {
-
+        assertEquals(cercleTest.description(3), "   Cercle centre= 50.0, 40.0 r= 25.0");
     }
 
     @Test
     public void testDupliquer() {
-
+        IForme cercleDup = cercleTest.dupliquer();
+        // Utilisation de centre pour vérifier les valeurs du point
+        assertEquals(cercleTest.centre(), cercleDup.centre());
+        assertEquals(rayonCerc, cercleDup.largeur()/2, 0.0001);
     }
 
     @Test
