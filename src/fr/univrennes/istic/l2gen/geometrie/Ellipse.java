a393e63d870f8bfd8bf68bdf35b42a4203aba182
cecilia duchene <cduchene@d024m06.istic.univ-rennes1.fr>
2024-02-15 16:10:38 +0100

    modification d'elipse

--- src/fr/univrennes/istic/l2gen/geometrie/Ellipse.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Ellipse.java
@@ -1,19 +1,22 @@
 package fr.univrennes.istic.l2gen.geometrie;
 
+import java.awt.Point;
+
 public class Ellipse implements IForme {
 
     private Point centre;
     private double hauteur;
     private double largeur;
+    private double rayon;
 
-    public Ellipse(double centre1, double centre2, double hauteur, double largeur) {
-        this.centre = new Point(centre1, centre2);
+    public Ellipse(double x, double y, double hauteur, double largeur) {
+        this.centre = new Point(x, y);
         this.largeur = largeur;
         this.hauteur = hauteur;
     }
 
-    public Ellipse(Point centre, double hauteur, double largeur) {
-        this.centre = centre;
+    public Ellipse(Point p, double hauteur, double largeur) {
+        this.centre = p;
         this.hauteur = hauteur;
         this.largeur = largeur;
     }
@@ -43,11 +46,28 @@ public class Ellipse implements IForme {
                 ", hauteur " + hauteur +
                 ", largeur " + largeur + ". ";
         if (entier > 0) {
-            description += "C'est une ellipse particulière.";
+            description += "C'est une ellipse particuliere.";
         } else {
             description += "C'est une ellipse standard.";
         }
         return description;
+    }
+
+    public void redimmensioner(double largeur, double hauteur) {
+        this.rayon = rayon * largeur;
+    }
+
+    public void deplacer(double dx, double dy) {
+        this.centre = this.centre.plus(dx, dy);
+    }
+
+   
+    public IForme dupliquer() {
+        return new Ellipse(centre, hauteur, largeur);
+    }
+
+    public String enSVG() {
+        return "<ellipse cx= " + centre.x + " cy= " + centre.y + " rx= " + hauteur + " ry= " + largeur + "fill=\"white\"" + " stroke=\"black\"/>";
     }
 
     public void colorier(String... couleurs) {
