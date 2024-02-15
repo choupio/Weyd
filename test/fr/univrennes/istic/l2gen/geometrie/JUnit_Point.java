c2291e5dc812bb43a88b6583aa48e5856b335897
@apaillier <alan.paillier@etudiant.univ-rennes1.fr>
2024-02-15 16:22:53 +0100

    modification de Junit_Point.java

--- test/fr/univrennes/istic/l2gen/geometrie/JUnit_Point.java
+++ test/fr/univrennes/istic/l2gen/geometrie/JUnit_Point.java
@@ -2,27 +2,41 @@ package test.fr.univrennes.istic.l2gen.geometrie;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Before;
 import org.junit.Test;
 
 import fr.univrennes.istic.l2gen.geometrie.Point;
 
 public class JUnit_Point {
+    Point p;
     @Before
     public void setUp(){
         p=new Point(5, 6);
     }
+    
     @Test
-    public void testx(){
-
+    public void testX(){
+        assertEquals(5,p.x(),0.0001);
     }
     
     @Test
-    public void testy(){
-
+    public void testY(){
+        assertEquals(6,p.y(),0.0001);
     }
     @Test
-    public void testsetX(){
-
+    public void testSetX(){
+        p.setX(9);
+        assertEquals(9,p.x(),0.0001);
     }
+    @Test
+    public void testSetY(){
+        p.setY(10);
+        assertEquals(10,p.y(),0.0001);
+    }
+    @Test
+    public void testEquals(){
+        
+    }
+
 }
