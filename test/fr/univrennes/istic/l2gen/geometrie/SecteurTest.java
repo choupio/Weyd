cedfcb259f72e8395f8927ced502cde2c1bc3df1
ankouame <ange.kouame@etudiant.univ-rennes1.fr>
2024-02-18 19:27:51 +0100

    réalisation de la classe test du secteur

--- test/fr/univrennes/istic/l2gen/geometrie/SecteurTest.java
+++ test/fr/univrennes/istic/l2gen/geometrie/SecteurTest.java
@@ -1,80 +1,105 @@
 package fr.univrennes.istic.l2gen.geometrie;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class SecteurTest {
+    Secteur s;
+    @Before
+    public void setUp(){
+        s = new Secteur(256 , 256 , 128 , 0 , 60);
+    }
+
+
     @Test
     public void testCentre() {
-
+        assertEquals(256, s.centre().x(), 0.001);
+        assertEquals(256, s.centre().y(), 0.001);
     }
 
     @Test
     public void testColorier() {
-
+        s.colorier("red");
+        assertEquals("red", s);
     }
 
     @Test
     public void testDeplacer() {
-
+        IForme secteurDeplace = s.deplacer(5, 10);
+        assertEquals(261, secteurDeplace.centre().x(), 0.001);
+        assertEquals(266, secteurDeplace.centre().y(), 0.001);
     }
 
     @Test
     public void testDescription() {
-
+        String descriptionAttendue = "Secteur centre=256.0,256.0 Angle=30.0 Arc=60.0";
+        assertEquals(descriptionAttendue, s.description(0));
     }
 
     @Test
     public void testDupliquer() {
-
+        IForme secteurDuplique = s.dupliquer();
+        assertEquals(s.getPoint().x(), ((Secteur) secteurDuplique).getPoint().x(), 0.001);
+        assertEquals(s.getPoint().y(), ((Secteur) secteurDuplique).getPoint().y(), 0.001);
+        assertEquals(s.getRayon(), ((Secteur) secteurDuplique).getRayon(), 0.001);
+        assertEquals(60, ((Secteur) secteurDuplique).getAngle(), 0.001);
+        assertEquals(s.getArc(), ((Secteur) secteurDuplique).getArc(), 0.001);
     }
 
     @Test
     public void testEnSVG() {
-
+        String svgAttendu = "<path d=\"M 366.85125168440817 192.0 A 128.0 128.0 0 0 0 256.0 128.0 L 256.0 256.0 Z\"\n" +
+        "\t" + "fill=\"white\"" + " stroke=\"black\"/>";
+        assertEquals(svgAttendu, s.enSVG());
     }
 
     @Test
     public void testGetAngle() {
-
+        assertEquals(30, s.getAngle(), 0.001);
     }
 
     @Test
     public void testGetArc() {
-
+        assertEquals(60, s.getArc(), 0.001);
     }
 
     @Test
     public void testGetPoint() {
-
+        assertEquals(new Point(256, 256), s.getPoint());
     }
 
     @Test
     public void testGetRayon() {
-
+        assertEquals(128, s.getRayon(), 0.001);
     }
 
     @Test
     public void testHauteur() {
-
+        assertEquals(0, s.hauteur(), 0.001);
     }
 
     @Test
     public void testLargeur() {
-
+        assertEquals(0, s.largeur(), 0.001);
     }
 
     @Test
     public void testRedimmensioner() {
-
+        IForme secteurRedimensionne = s.redimmensioner(0, 0);
+        assertEquals(0*0, secteurRedimensionne.hauteur() * secteurRedimensionne.largeur(), 0.001);
     }
 
     @Test
     public void testSetAngle() {
-
+        s.setAngle(60);
+        assertEquals(60, s.getAngle(), 0.001);
     }
 
     @Test
     public void testSetArc() {
-
+        s.setArc(120);
+        assertEquals(120, s.getArc(), 0.001);
     }
 }
