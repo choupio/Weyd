4a83fb423fb35a664a2d2990de09fae38c326435
ankouame <ange.kouame@etudiant.univ-rennes1.fr>
2024-02-19 23:46:34 +0100

    suppression de lignes inutiles

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import fr.univrennes.istic.l2gen.geometrie.Cercle;
 import fr.univrennes.istic.l2gen.geometrie.Ellipse;
 import fr.univrennes.istic.l2gen.geometrie.Groupe;
-import fr.univrennes.istic.l2gen.geometrie.Hexagone;
+//import fr.univrennes.istic.l2gen.geometrie.Hexagone;
 import fr.univrennes.istic.l2gen.geometrie.Ligne;
 import fr.univrennes.istic.l2gen.geometrie.Polygone;
 import fr.univrennes.istic.l2gen.geometrie.Rectangle;
@@ -10,9 +10,7 @@ import fr.univrennes.istic.l2gen.geometrie.Triangle;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Polygone r1 = new Polygone(128, 128, 128, 256, 256, 256, 256, 128);
-        r1.colorier("red");
-        System.out.println(r1.enSVG());
+
         Groupe tableau = new Groupe ();
         tableau . ajouter ( new Cercle (256 , 256 , 128));
         tableau . ajouter ( new Ellipse (256 , 256 , 128 , 64));
@@ -22,6 +20,6 @@ public class App {
         tableau . ajouter ( new Secteur (256 , 256 , 128 , 0 , 60));
         tableau . ajouter ( new Triangle (192 , 128 , 256 , 128 , 256 , 256));
         tableau.createSvgFile();
-        new Rectangle (256 , 256 , 256 , 128).createSvgFile();
+
     }
 }
