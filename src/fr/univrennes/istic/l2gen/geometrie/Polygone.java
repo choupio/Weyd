8c0aef3c8b41789bf61988380f192aab425393db
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-02-20 18:58:39 +0100

    correction de description, deplacer et redimensionner de Polygone

--- src/fr/univrennes/istic/l2gen/geometrie/Polygone.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Polygone.java
@@ -18,7 +18,7 @@ public class Polygone implements IForme {
     public Polygone(double... d) {
         this.points = new ArrayList<>();
         for (int i = 1; i < d.length; i += 2) {
-            points.add(new Point(d[i], d[i - 1]));
+            points.add(new Point(d[i - 1], d[i]));
         }
     }
 
@@ -50,11 +50,11 @@ public class Polygone implements IForme {
             cran += "  ";
         }
 
-        String s = "Polygone";
+        String s = "Polygone" + cran;
         for (Point point : points) {
-            s += point.x() + "," + point.y();
+            s += point.x() + "," + point.y() + " ";
         }
-        return s + colorier(couleur);
+        return s;
     }
 
     @Override
@@ -91,8 +91,9 @@ public class Polygone implements IForme {
 
     @Override
     public IForme deplacer(double dx, double dy) {
-        for (Point point : points) {
-            point = new Point(dx + point.x(), dy + point.y());
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.remove(i);
+            points.add(i, new Point(dx + point.x(), dy + point.y()));
         }
         return this;
     }
@@ -100,20 +101,24 @@ public class Polygone implements IForme {
     @Override
     public IForme dupliquer() {
         // Crée une nouvelle instance de la classe avec les mêmes propriétés
-        Polygone nouvelleForme = new  Polygone();
+        Polygone nouvelleForme = new Polygone();
         for (Point point : points) {
             nouvelleForme.ajouterSommet(point);
         }
-        nouvelleForme.couleur = this.couleur;  // Copie de la couleur, ajustez selon vos besoins
+        nouvelleForme.couleur = this.couleur; // Copie de la couleur, ajustez selon vos besoins
         return nouvelleForme;
     }
 
     @Override
     public IForme redimmensioner(double h, double l) {
-        for (Point point : points) {
-            Point centre = this.centre();
-            double distance = Math.sqrt(Math.pow(centre.x() - point.x(), 2) + Math.pow(centre.y(), point.y()));
-            point = new Point(point.x() + distance * (1 - h), point.y() + distance * (1 - l));
+        Point centre = this.centre();
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.remove(i);
+            // double distance = Math.sqrt(Math.pow(centre.x() - point.x(), 2) +
+            // Math.pow(centre.y() - point.y(), 2));
+            double distanceX = centre.x() - point.x();
+            double distanceY = centre.y() - point.y();
+            points.add(i, new Point(centre.x() - distanceX * h, centre.y() - distanceY * l));
         }
         return this;
     }
@@ -133,11 +138,12 @@ public class Polygone implements IForme {
         couleur = couleurs[0];
         return this;
     }
-        public void createSvgFile() {
-    String svgContent = "<svg xmlns=\"http://www.w3.org/2000/svg\">\n";
-    
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/l2gen_5_coupdumarteau/src/fr/univrennes/istic/l2gen/geometrie/Polygone.svg"))) {
+    public void createSvgFile() {
+        String svgContent = "<svg xmlns=\"http://www.w3.org/2000/svg\">\n";
+
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter("/l2gen_5_coupdumarteau/src/fr/univrennes/istic/l2gen/geometrie/Polygone.svg"))) {
             writer.write(svgContent);
             writer.write(enSVG());
             writer.write("</svg>");
