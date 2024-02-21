3861f70a3c3b29e9af595d6ee8d89cb8a5647882
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-02-21 22:13:53 +0100

    ajout de tests pour tourner

--- src/fr/univrennes/istic/l2gen/geometrie/Polygone.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Polygone.java
@@ -11,216 +11,231 @@ import java.util.List;
  */
 public class Polygone implements IForme {
 
-    public List<Point> getSommets() {
-        return points;
-    }
+	public List<Point> getSommets() {
+		return points;
+	}
 
-    private List<Point> points; // Liste des sommets du polygone
-    private String couleur = "white"; // Couleur du polygone en "white"
+	private List<Point> points; // Liste des sommets du polygone
+	private String couleur = "white"; // Couleur du polygone en "white"
 
-    /**
-     * Constructeur de la classe Polygone prenant en paramètre les coordonnées des sommets du polygone.
-     *
-     * @param d Les coordonnées des sommets du polygone. Chaque paire de valeurs consécutives représente les coordonnées x et y d'un sommet.
-     */
-    public Polygone(double... d) {
-        this.points = new ArrayList<>();
-        for (int i = 1; i < d.length; i += 2) {
-            points.add(new Point(d[i - 1], d[i]));
-        }
-    }
+	/**
+	 * Constructeur de la classe Polygone prenant en paramètre les coordonnées des
+	 * sommets du polygone.
+	 *
+	 * @param d Les coordonnées des sommets du polygone. Chaque paire de valeurs
+	 *          consécutives représente les coordonnées x et y d'un sommet.
+	 */
+	public Polygone(double... d) {
+		this.points = new ArrayList<>();
+		for (int i = 1; i < d.length; i += 2) {
+			points.add(new Point(d[i - 1], d[i]));
+		}
+	}
 
-    /**
-     * Ajoute un sommet au polygone.
-     *
-     * @param point Le sommet à ajouter au polygone.
-     * @return Une référence à l'instance actuelle du polygone, pour permettre les opérations en chaîne.
-     */
-    public IForme ajouterSommet(Point point) {
-        points.add(new Point(point.x(), point.y()));
-        return this;
-    }
+	/**
+	 * Ajoute un sommet au polygone.
+	 *
+	 * @param point Le sommet à ajouter au polygone.
+	 * @return Une référence à l'instance actuelle du polygone, pour permettre les
+	 *         opérations en chaîne.
+	 */
+	public IForme ajouterSommet(Point point) {
+		points.add(new Point(point.x(), point.y()));
+		return this;
+	}
 
-    /**
-     * Ajoute un sommet au polygone en spécifiant ses coordonnées.
-     *
-     * @param d1 La coordonnée x du sommet à ajouter.
-     * @param d2 La coordonnée y du sommet à ajouter.
-     * @return Une référence à l'instance actuelle du polygone, pour permettre les opérations en chaîne.
-     */
-    public IForme ajouterSommet(double d1, double d2) {
-        points.add(new Point(d1, d2));
-        return this;
-    }
+	/**
+	 * Ajoute un sommet au polygone en spécifiant ses coordonnées.
+	 *
+	 * @param d1 La coordonnée x du sommet à ajouter.
+	 * @param d2 La coordonnée y du sommet à ajouter.
+	 * @return Une référence à l'instance actuelle du polygone, pour permettre les
+	 *         opérations en chaîne.
+	 */
+	public IForme ajouterSommet(double d1, double d2) {
+		points.add(new Point(d1, d2));
+		return this;
+	}
 
-    /**
-     * Retourne le centre du polygone, calculé comme la moyenne des coordonnées de ses sommets.
-     *
-     * @return Le centre du polygone.
-     */
-    @Override
-    public Point centre() {
-        double x = 0, y = 0;
-        for (Point point : points) {
-            x += point.x();
-            y += point.y();
-        }
-        return new Point(x / points.size(), y / points.size());
-    }
+	/**
+	 * Retourne le centre du polygone, calculé comme la moyenne des coordonnées de
+	 * ses sommets.
+	 *
+	 * @return Le centre du polygone.
+	 */
+	@Override
+	public Point centre() {
+		double x = 0, y = 0;
+		for (Point point : points) {
+			x += point.x();
+			y += point.y();
+		}
+		return new Point(x / points.size(), y / points.size());
+	}
 
-    /**
-     * Retourne une description du polygone avec une indentation spécifiée.
-     *
-     * @param entier Le niveau d'indentation pour la description.
-     * @return Une chaîne de caractères décrivant le polygone.
-     */
-    @Override
-    public String description(int entier) {
-        String cran = "";
-        for (int i = 0; i < entier; i += 1) {
-            cran += "  ";
-        }
+	/**
+	 * Retourne une description du polygone avec une indentation spécifiée.
+	 *
+	 * @param entier Le niveau d'indentation pour la description.
+	 * @return Une chaîne de caractères décrivant le polygone.
+	 */
+	@Override
+	public String description(int entier) {
+		String cran = "";
+		for (int i = 0; i < entier; i += 1) {
+			cran += "  ";
+		}
 
-        String s = cran + "Polygone ";
-        for (Point point : points) {
-            s += point.x() + "," + point.y() + " ";
-        }
-        s = s.substring(0, s.length() - 1); // supprime le dernier caractere
-        return s;
-    }
+		String s = cran + "Polygone ";
+		for (Point point : points) {
+			s += point.x() + "," + point.y() + " ";
+		}
+		s = s.substring(0, s.length() - 1); // supprime le dernier caractere
+		return s;
+	}
 
-    /**
-     * Retourne la hauteur du polygone, calculée comme la différence entre la coordonnée y maximale et la coordonnée y minimale parmi tous les sommets.
-     *
-     * @return La hauteur du polygone.
-     */
-    @Override
-    public double hauteur() {
-        Point mini = points.get(0), max = points.get(0);
-        for (Point point : points) {
-            if (point.y() < mini.y()) {
-                mini = point;
-            }
-            if (point.y() > max.y()) {
-                max = point;
-            }
-        }
-        return max.y() - mini.y();
-    }
+	/**
+	 * Retourne la hauteur du polygone, calculée comme la différence entre la
+	 * coordonnée y maximale et la coordonnée y minimale parmi tous les sommets.
+	 *
+	 * @return La hauteur du polygone.
+	 */
+	@Override
+	public double hauteur() {
+		Point mini = points.get(0), max = points.get(0);
+		for (Point point : points) {
+			if (point.y() < mini.y()) {
+				mini = point;
+			}
+			if (point.y() > max.y()) {
+				max = point;
+			}
+		}
+		return max.y() - mini.y();
+	}
 
-    /**
-     * Retourne la largeur du polygone, calculée comme la différence entre la coordonnée x maximale et la coordonnée x minimale parmi tous les sommets.
-     *
-     * @return La largeur du polygone.
-     */
-    @Override
-    public double largeur() {
-        Point mini = points.get(0), max = points.get(0);
-        for (Point point : points) {
-            if (point.x() < mini.x()) {
-                mini = point;
-            }
-            if (point.x() > max.x()) {
-                max = point;
-            }
-        }
-        return max.x() - mini.x();
-    }
+	/**
+	 * Retourne la largeur du polygone, calculée comme la différence entre la
+	 * coordonnée x maximale et la coordonnée x minimale parmi tous les sommets.
+	 *
+	 * @return La largeur du polygone.
+	 */
+	@Override
+	public double largeur() {
+		Point mini = points.get(0), max = points.get(0);
+		for (Point point : points) {
+			if (point.x() < mini.x()) {
+				mini = point;
+			}
+			if (point.x() > max.x()) {
+				max = point;
+			}
+		}
+		return max.x() - mini.x();
+	}
 
-    /**
-     * Déplace le polygone selon les déplacements spécifiés.
-     *
-     * @param dx Le déplacement en abscisse.
-     * @param dy Le déplacement en ordonnée.
-     * @return Une référence à l'instance du polygone, pour permettre les opérations en chaîne.
-     */
-    @Override
-    public IForme deplacer(double dx, double dy) {
-        for (int i = 0; i < points.size(); i++) {
-            Point point = points.remove(i);
-            points.add(i, new Point(dx + point.x(), dy + point.y()));
-        }
-        return this;
-    }
+	/**
+	 * Déplace le polygone selon les déplacements spécifiés.
+	 *
+	 * @param dx Le déplacement en abscisse.
+	 * @param dy Le déplacement en ordonnée.
+	 * @return Une référence à l'instance du polygone, pour permettre les opérations
+	 *         en chaîne.
+	 */
+	@Override
+	public IForme deplacer(double dx, double dy) {
+		for (int i = 0; i < points.size(); i++) {
+			Point point = points.remove(i);
+			points.add(i, new Point(dx + point.x(), dy + point.y()));
+		}
+		return this;
+	}
 
-    /**
-     * Duplique le polygone.
-     *
-     * @return Une nouvelle instance du polygone avec les mêmes propriétés.
-     */
-    @Override
-    public IForme dupliquer() {
-        // Crée une nouvelle instance de la classe avec les mêmes propriétés
-        Polygone nouvelleForme = new Polygone();
-        for (Point point : points) {
-            nouvelleForme.ajouterSommet(point);
-        }
-        nouvelleForme.couleur = this.couleur; // Copie de la couleur, ajustez selon vos besoins
-        return nouvelleForme;
-    }
+	/**
+	 * Duplique le polygone.
+	 *
+	 * @return Une nouvelle instance du polygone avec les mêmes propriétés.
+	 */
+	@Override
+	public IForme dupliquer() {
+		// Crée une nouvelle instance de la classe avec les mêmes propriétés
+		Polygone nouvelleForme = new Polygone();
+		for (Point point : points) {
+			nouvelleForme.ajouterSommet(point);
+		}
+		nouvelleForme.couleur = this.couleur; // Copie de la couleur, ajustez selon vos besoins
+		return nouvelleForme;
+	}
 
-    /**
-     * Redimensionne le polygone selon les dimensions spécifiées.
-     *
-     * @param h La hauteur de redimensionnement.
-     * @param l La largeur de redimensionnement.
-     * @return Une référence à l'instance du polygone, pour permettre les opérations en chaîne.
-     */
-    @Override
-    public IForme redimmensioner(double h, double l) {
-        Point centre = this.centre();
-        for (int i = 0; i < points.size(); i++) {
-            Point point = points.remove(i);
-            double distanceX = centre.x() - point.x();
-            double distanceY = centre.y() - point.y();
-            points.add(i, new Point(centre.x() - distanceX * h, centre.y() - distanceY * l));
-        }
-        return this;
-    }
+	/**
+	 * Redimensionne le polygone selon les dimensions spécifiées.
+	 *
+	 * @param h La hauteur de redimensionnement.
+	 * @param l La largeur de redimensionnement.
+	 * @return Une référence à l'instance du polygone, pour permettre les opérations
+	 *         en chaîne.
+	 */
+	@Override
+	public IForme redimmensioner(double h, double l) {
+		Point centre = this.centre();
+		for (int i = 0; i < points.size(); i++) {
+			Point point = points.remove(i);
+			double distanceX = centre.x() - point.x();
+			double distanceY = centre.y() - point.y();
+			points.add(i, new Point(centre.x() - distanceX * h, centre.y() - distanceY * l));
+		}
+		return this;
+	}
 
-    /**
-     * Génère une représentation SVG du polygone.
-     *
-     * @return Une chaîne de caractères représentant le polygone en format SVG.
-     */
-    @Override
-    public String enSVG() {
-        String s = "<polygon points=\"";
-        for (Point point : points) {
-            s += point.x() + " " + point.y() + " ";
-        }
-        s = s.substring(0, s.length() - 1); // supprime le dernier caractere
-        s += "\" fill=\"" + couleur + "\" stroke=\"black\"/>";
-        return s;
-    }
+	/**
+	 * Génère une représentation SVG du polygone.
+	 *
+	 * @return Une chaîne de caractères représentant le polygone en format SVG.
+	 */
+	@Override
+	public String enSVG() {
+		String s = "<polygon points=\"";
+		for (Point point : points) {
+			s += point.x() + " " + point.y() + " ";
+		}
+		s = s.substring(0, s.length() - 1); // supprime le dernier caractere
+		s += "\" fill=\"" + couleur + "\" stroke=\"black\"/>";
+		return s;
+	}
 
-    /**
-     * Change la couleur du polygone.
-     *
-     * @param couleurs Un tableau de couleurs à appliquer au polygone.
-     * @return Une référence à l'instance du polygone, pour permettre les opérations en chaîne.
-     */
-    @Override
-    public IForme colorier(String... couleurs) {
-        this.couleur = couleurs[0];
-        return this;
-    }
+	/**
+	 * Change la couleur du polygone.
+	 *
+	 * @param couleurs Un tableau de couleurs à appliquer au polygone.
+	 * @return Une référence à l'instance du polygone, pour permettre les opérations
+	 *         en chaîne.
+	 */
+	@Override
+	public IForme colorier(String... couleurs) {
+		this.couleur = couleurs[0];
+		return this;
+	}
 
-    /**
-     * Crée un fichier SVG représentant le polygone.
-     */
-    public void createSvgFile() {
-        String svgContent = "<svg xmlns=\"http://www.w3.org/2000/svg\">\n";
+	/**
+	 * Crée un fichier SVG représentant le polygone.
+	 */
+	public void createSvgFile() {
+		String svgContent = "<svg xmlns=\"http://www.w3.org/2000/svg\">\n";
 
-        try (BufferedWriter writer = new BufferedWriter(
-                new FileWriter("/l2gen_5_coupdumarteau/src/fr/univrennes/istic/l2gen/geometrie/Polygone.svg"))) {
-            writer.write(svgContent);
-            writer.write(enSVG());
-            writer.write("</svg>");
-            System.out.println("Fichier créé avec succès !");
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la création du fichier : " + e.getMessage());
-        }
-    }
+		try (BufferedWriter writer = new BufferedWriter(
+				new FileWriter("/l2gen_5_coupdumarteau/src/fr/univrennes/istic/l2gen/geometrie/Polygone.svg"))) {
+			writer.write(svgContent);
+			writer.write(enSVG());
+			writer.write("</svg>");
+			System.out.println("Fichier créé avec succès !");
+		} catch (IOException e) {
+			System.err.println("Erreur lors de la création du fichier : " + e.getMessage());
+		}
+	}
+
+	public IForme tourner(int angle) {
+
+		return this;
+	}
 
 }
--- test/fr/univrennes/istic/l2gen/geometrie/PolygoneTest.java
+++ test/fr/univrennes/istic/l2gen/geometrie/PolygoneTest.java
@@ -10,154 +10,165 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PolygoneTest {
-    private Polygone polygone, polygone2;
+	private Polygone polygone, polygone2;
 
-    @Before
-    public void setUp() {
-        polygone = new Polygone();
+	@Before
+	public void setUp() {
+		polygone = new Polygone();
 
-        polygone2 = new Polygone();
-        polygone2.ajouterSommet(128, 128);
-        polygone2.ajouterSommet(128, 256);
-        polygone2.ajouterSommet(256, 128);
-        polygone2.ajouterSommet(256, 256);
-    }
+		polygone2 = new Polygone();
+		polygone2.ajouterSommet(128, 128);
+		polygone2.ajouterSommet(128, 256);
+		polygone2.ajouterSommet(256, 128);
+		polygone2.ajouterSommet(256, 256);
+	}
 
-    @Test
-    public void testAjouterSommet() {
-        assertEquals(Collections.emptyList(), polygone.getSommets());
-        double d1, d2;
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            d1 = random.nextInt(200);
-            d2 = random.nextInt(200);
-            polygone.ajouterSommet(d1, d2);
-            assertEquals(i + 1, polygone.getSommets().size());
-            assertTrue(polygone.getSommets().get(i).equals(new Point(d1, d2)));
-        }
-    }
+	@Test
+	public void testAjouterSommet() {
+		assertEquals(Collections.emptyList(), polygone.getSommets());
+		double d1, d2;
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			d1 = random.nextInt(200);
+			d2 = random.nextInt(200);
+			polygone.ajouterSommet(d1, d2);
+			assertEquals(i + 1, polygone.getSommets().size());
+			assertTrue(polygone.getSommets().get(i).equals(new Point(d1, d2)));
+		}
+	}
 
-    @Test
-    public void testAjouterSommet2() {
-        assertEquals(Collections.emptyList(), polygone.getSommets());
-        double d1, d2;
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            d1 = random.nextInt(200);
-            d2 = random.nextInt(200);
-            polygone.ajouterSommet(new Point(d1, d2));
-            assertEquals(i + 1, polygone.getSommets().size());
-            assertTrue(polygone.getSommets().get(i).equals(new Point(d1, d2)));
-        }
-    }
+	@Test
+	public void testAjouterSommet2() {
+		assertEquals(Collections.emptyList(), polygone.getSommets());
+		double d1, d2;
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			d1 = random.nextInt(200);
+			d2 = random.nextInt(200);
+			polygone.ajouterSommet(new Point(d1, d2));
+			assertEquals(i + 1, polygone.getSommets().size());
+			assertTrue(polygone.getSommets().get(i).equals(new Point(d1, d2)));
+		}
+	}
 
-    @Test
-    public void testCentre() {
-        assertTrue(polygone2.centre().equals(new Point(192, 192)));
-    }
+	@Test
+	public void testCentre() {
+		assertTrue(polygone2.centre().equals(new Point(192, 192)));
+	}
 
-    @Test
-    public void testDeplacer() {
-        Polygone polygoneTest = new Polygone(138, 138, 138, 266, 266, 138, 266, 266);
-        polygone2.deplacer(10, 10);
+	@Test
+	public void testDeplacer() {
+		Polygone polygoneTest = new Polygone(138, 138, 138, 266, 266, 138, 266, 266);
+		polygone2.deplacer(10, 10);
 
-        for (int i = 0; i < polygoneTest.getSommets().size(); i++) {
-            assertTrue(polygoneTest.getSommets().get(i).equals(polygone2.getSommets().get(i)));
-        }
-    }
+		for (int i = 0; i < polygoneTest.getSommets().size(); i++) {
+			assertTrue(polygoneTest.getSommets().get(i).equals(polygone2.getSommets().get(i)));
+		}
+	}
 
-    @Test
-    public void testDescription() {
-        assertEquals("  Polygone 128.0,128.0 128.0,256.0 256.0,128.0 256.0,256.0", polygone2.description(1));
-    }
+	@Test
+	public void testDescription() {
+		assertEquals("  Polygone 128.0,128.0 128.0,256.0 256.0,128.0 256.0,256.0", polygone2.description(1));
+	}
 
-    @Test
-    public void testDupliquer() {
-        Polygone polygoneTest = (Polygone) polygone2.dupliquer();
-        for (int i = 0; i < polygoneTest.getSommets().size(); i++) {
-            assertTrue(polygoneTest.getSommets().get(i).equals(polygone2.getSommets().get(i)));
-        }
-    }
+	@Test
+	public void testDupliquer() {
+		Polygone polygoneTest = (Polygone) polygone2.dupliquer();
+		for (int i = 0; i < polygoneTest.getSommets().size(); i++) {
+			assertTrue(polygoneTest.getSommets().get(i).equals(polygone2.getSommets().get(i)));
+		}
+	}
 
-    @Test
-    public void testEnSVG() {
-        assertEquals(
-                "<polygon points=\"128.0 128.0 128.0 256.0 256.0 128.0 256.0 256.0\" fill=\"white\" stroke=\"black\"/>",
-                polygone2.enSVG());
-    }
+	@Test
+	public void testEnSVG() {
+		assertEquals(
+				"<polygon points=\"128.0 128.0 128.0 256.0 256.0 128.0 256.0 256.0\" fill=\"white\" stroke=\"black\"/>",
+				polygone2.enSVG());
+	}
 
-    @Test
-    public void testGetSommets() {
-        assertEquals(Collections.emptyList(), polygone.getSommets());
-        double d1, d2;
-        Random random = new Random();
-        ArrayList<Point> points = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            d1 = random.nextInt(200);
-            d2 = random.nextInt(200);
-            points.add(new Point(d1, d2));
-            polygone.ajouterSommet(new Point(d1, d2));
-        }
+	@Test
+	public void testGetSommets() {
+		assertEquals(Collections.emptyList(), polygone.getSommets());
+		double d1, d2;
+		Random random = new Random();
+		ArrayList<Point> points = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			d1 = random.nextInt(200);
+			d2 = random.nextInt(200);
+			points.add(new Point(d1, d2));
+			polygone.ajouterSommet(new Point(d1, d2));
+		}
 
-        for (int i = 0; i < points.size(); i++) {
-            assertTrue(points.get(i).equals(polygone.getSommets().get(i)));
-        }
-    }
+		for (int i = 0; i < points.size(); i++) {
+			assertTrue(points.get(i).equals(polygone.getSommets().get(i)));
+		}
+	}
 
-    @Test
-    public void testHauteur() {
-        polygone.ajouterSommet(0, 0);
-        polygone.ajouterSommet(0, 56);
-        assertEquals(56.0, polygone.hauteur(), 0.00001);
-    }
+	@Test
+	public void testHauteur() {
+		polygone.ajouterSommet(0, 0);
+		polygone.ajouterSommet(0, 56);
+		assertEquals(56.0, polygone.hauteur(), 0.00001);
+	}
 
-    @Test
-    public void testHauteur2() {
-        polygone.ajouterSommet(10, 32);
-        polygone.ajouterSommet(12, 56);
-        assertEquals(24, polygone.hauteur(), 0.00001);
-    }
+	@Test
+	public void testHauteur2() {
+		polygone.ajouterSommet(10, 32);
+		polygone.ajouterSommet(12, 56);
+		assertEquals(24, polygone.hauteur(), 0.00001);
+	}
 
-    @Test
-    public void testLargeur() {
-        polygone.ajouterSommet(0, 0);
-        polygone.ajouterSommet(0, 56);
-        assertEquals(0, polygone.largeur(), 0.00001);
-    }
+	@Test
+	public void testLargeur() {
+		polygone.ajouterSommet(0, 0);
+		polygone.ajouterSommet(0, 56);
+		assertEquals(0, polygone.largeur(), 0.00001);
+	}
 
-    @Test
-    public void testLargeur2() {
-        polygone.ajouterSommet(10, 32);
-        polygone.ajouterSommet(12, 56);
-        assertEquals(2, polygone.largeur(), 0.00001);
-    }
+	@Test
+	public void testLargeur2() {
+		polygone.ajouterSommet(10, 32);
+		polygone.ajouterSommet(12, 56);
+		assertEquals(2, polygone.largeur(), 0.00001);
+	}
 
-    @Test
-    public void testRedimmensioner() {
-        Polygone polygoneRef = new Polygone(160, 160, 160, 224, 224, 160, 224, 224);
+	@Test
+	public void testRedimmensioner() {
+		Polygone polygoneRef = new Polygone(160, 160, 160, 224, 224, 160, 224, 224);
 
-        Point centreDebut = polygone2.centre();
-        polygone2.redimmensioner(0.5, 0.5);
-        assertTrue(centreDebut.equals(polygone2.centre()));
-        for (int i = 0; i < polygoneRef.getSommets().size(); i++) {
-            assertEquals(polygoneRef.getSommets().get(i).x(), polygone2.getSommets().get(i).x(), 0.00001);
-            assertEquals(polygoneRef.getSommets().get(i).y(), polygone2.getSommets().get(i).y(), 0.00001);
-        }
+		Point centreDebut = polygone2.centre();
+		polygone2.redimmensioner(0.5, 0.5);
+		assertTrue(centreDebut.equals(polygone2.centre()));
+		for (int i = 0; i < polygoneRef.getSommets().size(); i++) {
+			assertEquals(polygoneRef.getSommets().get(i).x(), polygone2.getSommets().get(i).x(), 0.00001);
+			assertEquals(polygoneRef.getSommets().get(i).y(), polygone2.getSommets().get(i).y(), 0.00001);
+		}
 
-    }
+	}
 
-    @Test
-    public void testRedimmensioner2() {
-        Polygone polygoneRef = new Polygone(184, 184, 184, 200, 200, 184, 200, 200);
+	@Test
+	public void testRedimmensioner2() {
+		Polygone polygoneRef = new Polygone(184, 184, 184, 200, 200, 184, 200, 200);
 
-        Point centreDebut = polygone2.centre();
-        polygone2.redimmensioner(0.5, 0.5);
-        polygone2.redimmensioner(0.25, 0.25);
-        assertTrue(centreDebut.equals(polygone2.centre()));
-        for (int i = 0; i < polygoneRef.getSommets().size(); i++) {
-            assertEquals(polygoneRef.getSommets().get(i).x(), polygone2.getSommets().get(i).x(), 0.00001);
-            assertEquals(polygoneRef.getSommets().get(i).y(), polygone2.getSommets().get(i).y(), 0.00001);
-        }
+		Point centreDebut = polygone2.centre();
+		polygone2.redimmensioner(0.5, 0.5);
+		polygone2.redimmensioner(0.25, 0.25);
+		assertTrue(centreDebut.equals(polygone2.centre()));
+		for (int i = 0; i < polygoneRef.getSommets().size(); i++) {
+			assertEquals(polygoneRef.getSommets().get(i).x(), polygone2.getSommets().get(i).x(), 0.00001);
+			assertEquals(polygoneRef.getSommets().get(i).y(), polygone2.getSommets().get(i).y(), 0.00001);
+		}
+	}
 
-    }
+	@Test
+	public void testTourner() { // sans modifié l'angle
+		polygone2.tourner(38);
+		assertEquals("  Polygone 128.0,128.0 128.0,256.0 256.0,128.0 256.0,256.0 angle=38", polygone2.description(1));
+	}
+
+	@Test
+	public void testTourner2() { // en modifiant l'angle
+		polygone2.tourner(38);
+		assertEquals("  Polygone 128.0,128.0 128.0,256.0 256.0,128.0 256.0,256.0 angle=38", polygone2.description(1));
+	}
 }
