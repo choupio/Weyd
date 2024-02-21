a322be79a7d409a29f57c66514bcacd95b401dfa
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-02-21 20:09:38 +0100

    Ajout d'un @Before

--- test/fr/univrennes/istic/l2gen/geometrie/TexteTest.java
+++ test/fr/univrennes/istic/l2gen/geometrie/TexteTest.java
@@ -1,8 +1,16 @@
 package fr.univrennes.istic.l2gen.geometrie;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TexteTest {
+    private Texte texte;
+
+    @Before
+    public void setUP() {
+        texte = new Texte(192, 128, 64, "Istic L2GEN");
+    }
+
     @Test
     public void testCentre() {
 
