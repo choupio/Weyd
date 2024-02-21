742c5f667875e3edcebe79cef5238ff2653f8ee7
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-02-21 22:13:12 +0100

    ajout de touner() et aligner

--- src/fr/univrennes/istic/l2gen/geometrie/IForme.java
+++ src/fr/univrennes/istic/l2gen/geometrie/IForme.java
@@ -5,78 +5,98 @@ package fr.univrennes.istic.l2gen.geometrie;
  */
 public interface IForme {
 
-    /**
-     * Retourne le centre de la forme géométrique.
-     *
-     * @return Le centre de la forme géométrique.
-     */
-    public Point centre();
+	/**
+	 * Retourne le centre de la forme géométrique.
+	 *
+	 * @return Le centre de la forme géométrique.
+	 */
+	public Point centre();
 
-    /**
-     * Retourne une description de la forme géométrique, avec une indentation spécifiée.
-     *
-     * @param indentation Le niveau d'indentation pour la description.
-     * @return Une chaîne de caractères décrivant la forme géométrique.
-     */
-    public String description(int indentation);
+	/**
+	 * Retourne une description de la forme géométrique, avec une indentation
+	 * spécifiée.
+	 *
+	 * @param indentation Le niveau d'indentation pour la description.
+	 * @return Une chaîne de caractères décrivant la forme géométrique.
+	 */
+	public String description(int indentation);
 
-    /**
-     * Retourne la hauteur de la forme géométrique.
-     *
-     * @return La hauteur de la forme géométrique.
-     */
-    public double hauteur();
+	/**
+	 * Retourne la hauteur de la forme géométrique.
+	 *
+	 * @return La hauteur de la forme géométrique.
+	 */
+	public double hauteur();
 
-    /**
-     * Retourne la largeur de la forme géométrique.
-     *
-     * @return La largeur de la forme géométrique.
-     */
-    public double largeur();
+	/**
+	 * Retourne la largeur de la forme géométrique.
+	 *
+	 * @return La largeur de la forme géométrique.
+	 */
+	public double largeur();
 
-    /**
-     * Déplace la forme géométrique selon les déplacements spécifiés.
-     *
-     * @param dx Le déplacement en abscisse.
-     * @param dy Le déplacement en ordonnée.
-     * @return Une référence à l'instance de la forme géométrique, pour permettre les opérations en chaîne.
-     */
-    public IForme deplacer(double dx, double dy);
+	/**
+	 * Déplace la forme géométrique selon les déplacements spécifiés.
+	 *
+	 * @param dx Le déplacement en abscisse.
+	 * @param dy Le déplacement en ordonnée.
+	 * @return Une référence à l'instance de la forme géométrique, pour permettre
+	 *         les opérations en chaîne.
+	 */
+	public IForme deplacer(double dx, double dy);
 
-    /**
-     * Duplique la forme géométrique.
-     *
-     * @return Une nouvelle instance de la forme géométrique avec les mêmes propriétés.
-     */
-    public IForme dupliquer();
+	/**
+	 * Duplique la forme géométrique.
+	 *
+	 * @return Une nouvelle instance de la forme géométrique avec les mêmes
+	 *         propriétés.
+	 */
+	public IForme dupliquer();
 
-    /**
-     * Redimensionne la forme géométrique selon les dimensions spécifiées.
-     *
-     * @param h La hauteur de redimensionnement.
-     * @param l La largeur de redimensionnement.
-     * @return Une référence à l'instance de la forme géométrique, pour permettre les opérations en chaîne.
-     */
-    public IForme redimmensioner(double h, double l);
+	/**
+	 * Redimensionne la forme géométrique selon les dimensions spécifiées.
+	 *
+	 * @param h La hauteur de redimensionnement.
+	 * @param l La largeur de redimensionnement.
+	 * @return Une référence à l'instance de la forme géométrique, pour permettre
+	 *         les opérations en chaîne.
+	 */
+	public IForme redimmensioner(double h, double l);
 
-    /**
-     * Retourne une représentation SVG de la forme géométrique.
-     *
-     * @return Une chaîne de caractères représentant la forme géométrique en format SVG.
-     */
-    public String enSVG();
+	/**
+	 * Retourne une représentation SVG de la forme géométrique.
+	 *
+	 * @return Une chaîne de caractères représentant la forme géométrique en format
+	 *         SVG.
+	 */
+	public String enSVG();
 
-    /**
-     * Change la couleur de la forme géométrique.
-     *
-     * @param couleurs Un tableau de couleurs à appliquer à la forme géométrique.
-     * @return Une référence à l'instance de la forme géométrique, pour permettre les opérations en chaîne.
-     */
-    public IForme colorier(String... couleurs);
+	/**
+	 * Change la couleur de la forme géométrique.
+	 *
+	 * @param couleurs Un tableau de couleurs à appliquer à la forme géométrique.
+	 * @return Une référence à l'instance de la forme géométrique, pour permettre
+	 *         les opérations en chaîne.
+	 */
+	public IForme colorier(String... couleurs);
 
-    /**
-     * Crée un fichier SVG représentant la forme géométrique.
-     */
-    public void createSvgFile();
+	/**
+	 * Tourne la forme
+	 * 
+	 * @param angle l'angle de rotation
+	 * @return Une référence à l'instance de la forme géométrique, pour permettre
+	 *         les opérations en chaîne.
+	 */
+	public IForme tourner(int angle);
+
+	/***
+	 * @return Une référence à l'instance de la forme géométrique, pour permettre
+	 *         les opérations en chaîne.
+	 */
+	public IForme aligner();
+
+	/**
+	 * Crée un fichier SVG représentant la forme géométrique.
+	 */
+	public void createSvgFile();
 }
-
