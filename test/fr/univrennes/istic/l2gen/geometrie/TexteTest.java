b2abbe5b89427c342a3303427ea90a561369feb7
ankouame <ange.kouame@etudiant.univ-rennes1.fr>
2024-02-22 00:43:38 +0100

    modification de test en fonction des ajouts de text.java

--- test/fr/univrennes/istic/l2gen/geometrie/TexteTest.java
+++ test/fr/univrennes/istic/l2gen/geometrie/TexteTest.java
@@ -56,19 +56,19 @@ public class TexteTest {
 
 	@Test
 	public void testDescription() {
-		assertEquals("Texte centre=192.0,128.0 taille=64 texte=Istic L2GEN couleur=black", texte.description(0));
+		assertEquals("Texte centre=192.0,128.0 taille=64 texte=Istic L2GEN couleur=black et de rotation 0", texte.description(0));
 	}
 
 	@Test
 	public void testDescription2() {
-		assertEquals("  Texte centre=0.0,0.0 taille=0 texte= couleur=black", texteVide.description(1));
+		assertEquals("  Texte centre=0.0,0.0 taille=0 texte= couleur=black et de rotation 0", texteVide.description(1));
 	}
 
 	@Test
 	public void testDescription3() {
 		// test quand changement de couleur
 		texte.colorier("red");
-		assertEquals("Texte centre=192.0,128.0 taille=64 texte=Istic L2GEN couleur=red", texte.description(0));
+		assertEquals("Texte centre=192.0,128.0 taille=64 texte=Istic L2GEN couleur=red et de rotation 0", texte.description(0));
 	}
 
 	@Test
@@ -84,7 +84,7 @@ public class TexteTest {
 	@Test
 	public void testEnSVG() {
 		assertEquals(
-				"<text x=\"192.0\" y=\"128.0\" font-size=\"64\" text-anchor=\"middle\" fill=\"black\" stroke=\"black\">Istic L2GEN</text>",
+				"<text x=\"192.0\" y=\"128.0\" font-size=\"64\" text-anchor=\"middle\" fill=\"black\" stroke=\"black\" transform=\"rotate(0)\">Istic L2GEN</text>",
 				texte.enSVG());
 	}
 
@@ -92,29 +92,47 @@ public class TexteTest {
 	public void testEnSVG2() {// avec changement de couleur
 		texte.colorier("red");
 		assertEquals(
-				"<text x=\"192.0\" y=\"128.0\" font-size=\"64\" text-anchor=\"middle\" fill=\"red\" stroke=\"black\">Istic L2GEN</text>",
+				"<text x=\"192.0\" y=\"128.0\" font-size=\"64\" text-anchor=\"middle\" fill=\"red\" stroke=\"black\" transform=\"rotate(0)\">Istic L2GEN</text>",
 				texte.enSVG());
 	}
 
 	@Test
 	public void testEnSVG3() {
 		assertEquals(
-				"<text x=\"0.0\" y=\"0.0\" font-size=\"0\" text-anchor=\"middle\" fill=\"black\" stroke=\"black\"></text>",
+				"<text x=\"0.0\" y=\"0.0\" font-size=\"0\" text-anchor=\"middle\" fill=\"black\" stroke=\"black\" transform=\"rotate(0)\"></text>",
 				texteVide.enSVG());
 	}
 
 	@Test
 	public void testHauteur() {
-		// TODO
+		assertEquals(20.0, texte.hauteur(), 0.00001);
 	}
 
 	@Test
-	public void testLargeur() {
-		// TODO
+	public void alignergauche() {
+		texte.aligner(Alignement.GAUCHE, 0.0);
+		assertEquals(0, texte.centre().x(), 0.00001);
+		assertEquals(128, texte.centre().y(), 0.00001);
 	}
 
 	@Test
-	public void testRedimmensioner() {
-		// TODO
+	public void alignerdroite() {
+		texte.aligner(Alignement.DROITE, 0.0);
+		assertEquals(0.0, texte.centre().x(), 0.00001);
+		assertEquals(128, texte.centre().y(), 0.00001);
+	}
+
+	@Test
+	public void alignerhaut() {
+		texte.aligner(Alignement.HAUT, 0.0);
+		assertEquals(192, texte.centre().x(), 0.00001);
+		assertEquals(10.0, texte.centre().y(), 0.00001);
+	}
+
+	@Test
+	public void alignerbas() {
+		texte.aligner(Alignement.BAS, 0.0);
+		assertEquals(192, texte.centre().x(), 0.00001);
+		assertEquals(-10, texte.centre().y(), 0.00001);
 	}
 }
