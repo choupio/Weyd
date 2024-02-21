283537b216818eaad3ad0c1c9a94f92aecbd67a6
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-02-21 19:46:10 +0100

    Ajout du fichier test de Texte

---
+++ test/fr/univrennes/istic/l2gen/geometrie/TexteTest.java
@@ -0,0 +1,55 @@
+package fr.univrennes.istic.l2gen.geometrie;
+
+import org.junit.Test;
+
+public class TexteTest {
+    @Test
+    public void testCentre() {
+
+    }
+
+    @Test
+    public void testColorier() {
+
+    }
+
+    @Test
+    public void testCreateSvgFile() {
+
+    }
+
+    @Test
+    public void testDeplacer() {
+
+    }
+
+    @Test
+    public void testDescription() {
+
+    }
+
+    @Test
+    public void testDupliquer() {
+
+    }
+
+    @Test
+    public void testEnSVG() {
+
+    }
+
+    @Test
+    public void testHauteur() {
+
+    }
+
+    @Test
+    public void testLargeur() {
+
+    }
+
+    @Test
+    public void testRedimmensioner() {
+
+    }
+}
