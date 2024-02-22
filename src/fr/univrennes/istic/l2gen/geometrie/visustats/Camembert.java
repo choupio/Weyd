b05fd1d563bf953226e861a361f988c45284830c
cduchene <nathanael.brelaud@etudiant.univ-rennes.fr>
2024-02-22 13:08:12 +0100

    creation fonction dans camembert

---
+++ src/fr/univrennes/istic/l2gen/geometrie/visustats/Camembert.java
@@ -0,0 +1,96 @@
+package fr.univrennes.istic.l2gen.geometrie.visustats;
+
+import fr.univrennes.istic.l2gen.geometrie.Alignement;
+import fr.univrennes.istic.l2gen.geometrie.IForme;
+import fr.univrennes.istic.l2gen.geometrie.Point;
+
+public class Camembert implements IForme {
+
+    @Override
+    public Point centre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+    }
+    
+    public Camembert(Point point, double a){
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+    }   
+
+    public Camembert(double d, double c, double b){
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+    }
+
+    public Camembert ajouterSecteur(String s, double p){
+        throw new UnsupportedOperationException("Unimplemented method 'description'" + p + s);
+    }
+
+    @Override
+    public String description(int indentation) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'description'");
+    }
+
+    @Override
+    public double hauteur() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hauteur'");
+    }
+
+    @Override
+    public double largeur() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'largeur'");
+    }
+
+    @Override
+    public IForme deplacer(double dx, double dy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deplacer'");
+    }
+
+    @Override
+    public IForme dupliquer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dupliquer'");
+    }
+
+    @Override
+    public IForme redimmensioner(double h, double l) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'redimmensioner'");
+    }
+
+    @Override
+    public String enSVG() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'enSVG'");
+    }
+
+    @Override
+    public IForme colorier(String... couleurs) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'colorier'");
+    }
+
+    @Override
+    public IForme tourner(int angle) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tourner'");
+    }
+
+    @Override
+    public IForme aligner(Alignement alignement, double cible) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'aligner'");
+    }
+
+    @Override
+    public void createSvgFile() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createSvgFile'");
+    }
+
+    
+}
