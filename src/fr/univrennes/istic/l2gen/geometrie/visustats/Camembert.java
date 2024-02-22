c4db5d24c233058aae3bec5a7f54749a68cc00d1
cduchene <nathanael.brelaud@etudiant.univ-rennes.fr>
2024-02-22 13:44:05 +0100

    modif fonction dans camembert + secteur

--- src/fr/univrennes/istic/l2gen/geometrie/visustats/Camembert.java
+++ src/fr/univrennes/istic/l2gen/geometrie/visustats/Camembert.java
@@ -1,29 +1,36 @@
 package fr.univrennes.istic.l2gen.geometrie.visustats;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.univrennes.istic.l2gen.geometrie.Alignement;
 import fr.univrennes.istic.l2gen.geometrie.IForme;
 import fr.univrennes.istic.l2gen.geometrie.Point;
+import fr.univrennes.istic.l2gen.geometrie.Secteur;
 
 public class Camembert implements IForme {
+    private Point centre;
+    private double rayon;
+    private List<Secteur> secteurs;
 
     @Override
     public Point centre() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+        return centre;
     }
     
     public Camembert(Point point, double a){
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+        this.centre = centre;
+        this.rayon = rayon;
+        this.secteurs = new ArrayList<>();
     }   
 
-    public Camembert(double d, double c, double b){
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+    public Camembert(double x, double y, double rayon) {
+        this(new Point(x, y), rayon);
     }
 
-    public Camembert ajouterSecteur(String s, double p){
-        throw new UnsupportedOperationException("Unimplemented method 'description'" + p + s);
+    public Camembert ajouterSecteur(String description, double proportion) {
+        secteurs.add(new Secteur(description, proportion));
+        return this;
     }
 
     @Override
