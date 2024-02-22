e46b64d442232e38dfd58d51e093193f484bf2c2
@apaillier <alan.paillier@etudiant.univ-rennes1.fr>
2024-02-22 16:53:24 +0100

    modification de Ligne.java

--- src/fr/univrennes/istic/l2gen/geometrie/Ligne.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Ligne.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class Ligne implements IForme {
 
-    private Point[] ligne; // Tableau des sommets de la ligne
+    private List<Point> ligne; // Tableau des sommets de la ligne
     private String couleur = "black"; // Couleur de la ligne en "black"
     private int angle = 0;
 
@@ -24,12 +24,13 @@ public class Ligne implements IForme {
      *          consécutives représente les coordonnées x et y d'un sommet.
      */
     public Ligne(double... l) {
-        ligne = new Point[l.length / 2];
-        int j = 0;
-        for (int i = 0; i < l.length; i = i + 2) {
-            ligne[j] = new Point(l[i], l[i + 1]);
-            j++;
+        if(l.length % 2 ==0){
+            ligne = new ArrayList<>();
+            for (int i = 0; i < l.length; i = i + 2) {
+                ligne.add(new Point(l[i], l[i + 1]));
+            }
         }
+        
     }
 
     /**
@@ -40,12 +41,7 @@ public class Ligne implements IForme {
      *         opérations en chaîne.
      */
     public IForme ajouterSommet(Point p) {
-        Point[] temp = ligne;
-        ligne = new Point[temp.length + 1];
-        for (int i = 0; i < temp.length; i++) {
-            ligne[i] = temp[i];
-        }
-        ligne[ligne.length - 1] = p;
+        ligne.add(p);
         return this;
     }
 
@@ -69,7 +65,7 @@ public class Ligne implements IForme {
      * @return Le centre de la ligne.
      */
     public Point centre() {
-        return ligne[ligne.length - 1];
+        return ligne.getLast();
     }
 
     /**
@@ -85,8 +81,8 @@ public class Ligne implements IForme {
             identa += "  ";
         }
         result = identa + "Ligne";
-        for (int i = 0; i < ligne.length; i++) {
-            result = result + " " + ligne[i].x() + ',' + ligne[i].y();
+        for (int i = 0; i < ligne.size(); i++) {
+            result = result + " " + ligne.get(i).x() + ',' + ligne.get(i).y();
         }
         result += " de couleur " + couleur + " angle=" + angle;
         return result;
@@ -99,9 +95,9 @@ public class Ligne implements IForme {
      */
     public List<Double> getSommets() {
         List<Double> result = new ArrayList<>();
-        for (int i = 0; i < ligne.length; i++) {
-            result.add(ligne[i].x());
-            result.add(ligne[i].y());
+        for (int i = 0; i < ligne.size(); i++) {
+            result.add(ligne.get(i).x());
+            result.add(ligne.get(i).y());
         }
         return result;
     }
@@ -114,8 +110,8 @@ public class Ligne implements IForme {
      */
     public double hauteur() {
         List<Double> dy = new ArrayList<Double>();
-        for (int i = 0; i < ligne.length; i++) {
-            dy.add(ligne[i].y());
+        for (int i = 0; i < ligne.size(); i++) {
+            dy.add(ligne.get(i).y());
         }
         Double max = dy.get(0);
         Double min = dy.get(0);
@@ -138,8 +134,8 @@ public class Ligne implements IForme {
      */
     public double largeur() {
         List<Double> dx = new ArrayList<Double>();
-        for (int i = 0; i < ligne.length; i++) {
-            dx.add(ligne[i].x());
+        for (int i = 0; i < ligne.size(); i++) {
+            dx.add(ligne.get(i).x());
         }
         Double max = dx.get(0);
         Double min = dx.get(0);
@@ -161,9 +157,9 @@ public class Ligne implements IForme {
      */
     public String enSVG() {
         String result = "<polyline points=\"";
-        result = result + ligne[0].x() + ' ' + ligne[0].y() + ' ';
-        for (int i = 1; i < ligne.length; i++) {
-            result = result + ligne[i].y() + ' ' + ligne[i].y() + ' ';
+        result = result + ligne.get(0).x() + ' ' + ligne.get(0).y() + ' ';
+        for (int i = 1; i < ligne.size(); i++) {
+            result = result + ligne.get(i).y() + ' ' + ligne.get(i).y() + ' ';
         }
         result = result + "\" fill=\"white\" stroke=\"" + couleur + "\" transform=\"rotate(" + angle + ")\"/>";
         return result;
@@ -179,8 +175,8 @@ public class Ligne implements IForme {
      */
     @Override
     public IForme deplacer(double dx, double dy) {
-        for (int i = 0; i < ligne.length; i++) {
-            ligne[i] = new Point(ligne[i].x() + dx, ligne[i].y() + dy);
+        for (int i = 0; i < ligne.size(); i++) {
+            ligne.set(i, new Point(ligne.get(i).x() + dx, ligne.get(i).y() + dy));
         }
         return this;
     }
@@ -192,8 +188,8 @@ public class Ligne implements IForme {
      */
     public IForme dupliquer() {
         Ligne ligne2 = new Ligne();
-        for (int i = 0; i < ligne.length; i++) {
-            ligne2.ajouterSommet(ligne[i]);
+        for (int i = 0; i < ligne.size(); i++) {
+            ligne2.ajouterSommet(ligne.get(i));
         }
         ligne2.colorier(this.couleur);
         ligne2.tourner(this.angle);
@@ -210,8 +206,8 @@ public class Ligne implements IForme {
      */
     @Override
     public IForme redimmensioner(double h, double l) {
-        for (int i = 1; i < ligne.length; i++) {
-            ligne[i] = new Point(ligne[i].x() + h, ligne[i].y() + l);
+        for (int i = 1; i < ligne.size(); i++) {
+            ligne.set(i,new Point(ligne.get(i).x() + h, ligne.get(i).y() + l));
         }
         return this;
     }
