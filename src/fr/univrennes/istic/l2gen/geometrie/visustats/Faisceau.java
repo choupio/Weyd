5eb1c3d12330e396dfa6f7570bab30d43b71c3b6
etel leport <etleport@d024m07.istic.univ-rennes1.fr>
2024-02-22 16:26:01 +0100

    modifié tout Faisceau

--- src/fr/univrennes/istic/l2gen/geometrie/visustats/Faisceau.java
+++ src/fr/univrennes/istic/l2gen/geometrie/visustats/Faisceau.java
@@ -3,9 +3,12 @@ package fr.univrennes.istic.l2gen.geometrie.visustats;
 import java.util.List;
 import java.util.ArrayList;
 
+import fr.univrennes.istic.l2gen.geometrie.Alignement;
+import fr.univrennes.istic.l2gen.geometrie.IForme;
+import fr.univrennes.istic.l2gen.geometrie.Point;
 import fr.univrennes.istic.l2gen.geometrie.Rectangle;
 
-public class Faisceau {
+public class Faisceau implements IForme {
     private String nom;
     private List<Rectangle> barres;
 
@@ -16,6 +19,58 @@ public class Faisceau {
             Rectangle r = new Rectangle(0, 0, 1, h[i]);
             this.barres.add(r);
         }
+    }
+
+    public void agencer(double axeX, double axeY, double largeur, double echelle, boolean verticalement) {
+
+    }
+
+    public IForme colorier(String... couleurs) {
+
+    }
+
+    public IForme dupliquer() {
+        return this;
+    }
+
+    public Point centre() {
+        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+    }
+
+    public String description(int indentation) {
+        throw new UnsupportedOperationException("Unimplemented method 'description'");
+    }
+
+    public double hauteur() {
+        throw new UnsupportedOperationException("Unimplemented method 'hauteur'");
+    }
+
+    public double largeur() {
+        throw new UnsupportedOperationException("Unimplemented method 'largeur'");
+    }
+
+    public IForme deplacer() {
+        throw new UnsupportedOperationException("Unimplemented method 'deplacer'");
+    }
+
+    public IForme redimensionner() {
+        throw new UnsupportedOperationException("Unimplemented method 'redimensionner'");
+    }
+
+    public String enSVG() {
+        throw new UnsupportedOperationException("Unimplemented method 'enSVG'");
+    }
+
+    public IForme tourner(int angle) {
+        throw new UnsupportedOperationException("Unimplemented method 'tourner'");
+    }
+
+    public IForme aligner(Alignement alignement, double cible) {
+        throw new UnsupportedOperationException("Unimplemented method 'aligner'");
+    }
+
+    public void createSvgFile() {
+        throw new UnsupportedOperationException("Unimplemented method 'createSvgFile'");
     }
 
 }
