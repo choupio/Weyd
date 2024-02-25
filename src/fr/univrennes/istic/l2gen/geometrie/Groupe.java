52af52db9b9a6bf57993a5dcd9c124afc6120a3f
Sparenberg Maxance <maxance.sparenberg@univ-rennes1.fr>
2024-02-26 00:01:33 +0100

    enlèvement d'import inutiles

--- src/fr/univrennes/istic/l2gen/geometrie/Groupe.java
+++ src/fr/univrennes/istic/l2gen/geometrie/Groupe.java
@@ -1,8 +1,5 @@
 package fr.univrennes.istic.l2gen.geometrie;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
