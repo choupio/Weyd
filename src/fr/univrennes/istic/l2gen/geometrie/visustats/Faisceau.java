74b2f866d7077c1a749f0309357dcc73abfa9d82
Etel LEPORT <etel.leport@etudiant.univ-rennes1.fr>
2024-02-25 23:07:59 +0100

    Mofic faisceau

--- src/fr/univrennes/istic/l2gen/geometrie/visustats/Faisceau.java
+++ src/fr/univrennes/istic/l2gen/geometrie/visustats/Faisceau.java
@@ -79,27 +79,22 @@ public class Faisceau extends Groupe {
      * @param hauteur  La hauteur des rectangles.
      * @param vertical Indique si les rectangles doivent être alignés verticalement (true) ou horizontalement (false).
      */
-    public IForme agencer(double x, double y, double largeur, double hauteur, boolean vertical) {
-        Alignement alignement;
-        double cible;
-        double separation = 0.0;
-        if (largeur <=0 || axes.x() <0 || axes.y() <0){
+    public void agencer(double axeX, double axeY, double largeur, double echelle, boolean verticalement) {
+        axes.setX(axeX);
+        axes.setY(axeY);
+        this.largeur = largeur;
+        if (largeur <=0 || echelle <=0 || axeX <0 || axeY <0){
             throw new IllegalArgumentException("Les coordonée, l'echelle et la largeur ne peuvent pas être négatif.");
-        } else if (vertical) {
-            alignement = Alignement.HAUT;
-            cible = y;
-            separation = hauteur;
-            alignerElements(alignement, cible);
+        } else if (verticalement) {
+            this.empilerElements(Alignement.BAS, axeY,0);
+            this.alignerElements(Alignement.GAUCHE, axeY);
         } else {
-            empilerElements(Alignement.GAUCHE, axes.x(), separation);
-            alignement = Alignement.GAUCHE;
-            cible = x;
-            separation = largeur;
-            empilerElements(alignement, cible, separation);
+            this.alignerElements(Alignement.BAS, 500);
+            this.empilerElements(Alignement.GAUCHE, 500,10);
+            
         }
-        
-        return this;
     }
+
 
     
     /**
