d1c48bec17b2230a1efdbafd3bd911e8410fa23e
ankouame <ange.kouame@etudiant.univ-rennes1.fr>
2024-03-19 00:14:52 +0100

    suppression de code inutile

--- src/fr/univrennes/istic/l2gen/visustats/DiagCamemberts.java
+++ src/fr/univrennes/istic/l2gen/visustats/DiagCamemberts.java
@@ -168,12 +168,6 @@ public class DiagCamemberts implements IDataVisualiseur {
      */
     @Override
     public IForme colorier(String... couleurs) {
-        /*String temp = couleurs[1];
-        couleurs[1] = couleurs[3];
-        couleurs[3] = temp;
-        String temp1 = couleurs[0];
-        couleurs[0] = couleurs[4];
-        couleurs[4] = temp1;*/
         for (IForme forme : groupeCamembert.getListFormes()) {
             if (forme instanceof Camembert) {
                 Camembert camembert = (Camembert) forme;
@@ -242,22 +236,6 @@ public class DiagCamemberts implements IDataVisualiseur {
     }
 
 
-    /*public void echangerSecteurs(int index1, int index2) {
-        for (IForme forme : groupeCamembert.getListFormes()) {
-            if (forme instanceof Camembert) {
-                Camembert camembert = (Camembert) forme;
-                List<Secteur> secteurs = camembert.getSecteurs();
-                if (index1 < secteurs.size() && index2 < secteurs.size()) {
-                    // Échanger les secteurs
-                    Secteur temp = secteurs.get(index1);
-                    secteurs.set(index1, secteurs.get(index2));
-                    secteurs.set(index2, temp);
-                }
-            }
-        }
-    }*/
-
-
     /**
      * Ajoute les données spécifiées à l'objet IDataVisualiseur.
      * 
@@ -288,8 +266,7 @@ public class DiagCamemberts implements IDataVisualiseur {
             }
             groupeCamembert.ajouter(camembert);
         }
-        //echangerSecteurs(1, 3);
-
+        
         return this;   
     }
 
