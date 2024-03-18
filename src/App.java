cce9267fdd34be325da499154472ccadacdd9f20
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-03-18 20:24:15 +0100

    modif

--- src/App.java
+++ src/App.java
@@ -17,8 +17,8 @@ public class App {
         diag.ajouterDonnees("2010", 1600, 6800, 16000, 4300, 300);
         diag.ajouterDonnees("2015", 1900, 6600, 17500, 3800, 330);
         diag.ajouterDonnees("2020", 2100, 6200, 17800, 3600, 340);
-        diag.colorier("Blue ", " Green ", " Red ", " Yellow ", " Maroon ");
         diag.legender("Afrique", "Amerique", "Asie", "Europe", "Oceanie");
+        diag.colorier("Blue ", " Green ", " Red ", " Yellow ", " Maroon ");
         diag.agencer();
         diag.createSvgFile();
 
