fdc36c0bb873e6c9f896e11d493ed2dc783da42f
Etel LEPORT <etel.leport@etudiant.univ-rennes1.fr>
2024-03-19 18:47:29 +0100

    Ajout classe carburant

---
+++ src/fr/univrennes/istic/l2gen/visustats/Carburant.java
@@ -0,0 +1,14 @@
+package fr.univrennes.istic.l2gen.visustats;
+
+public class Carburant {
+    
+    private int prix;
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public Carburant(int prix){
+        this.prix = prix;
+    }
+}
