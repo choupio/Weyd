76eba1db2f6803f26810a2842d740f3a10781bf3
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-03-21 18:53:11 +0100

    ajout d'un bout de code pour tester le délire de carburant

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
 
 import fr.univrennes.istic.l2gen.geometrie.*;
+import fr.univrennes.istic.l2gen.station.Station;
+import fr.univrennes.istic.l2gen.station.StationParCarb;
 import fr.univrennes.istic.l2gen.visustats.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 import javax.swing.*;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Cette classe représente l'application principale.
@@ -99,14 +110,45 @@ public class App {
         // AccueilSwing s1 = new AccueilSwing();
 
         // Création d'un camembert
-        
-          Camembert camembert = new Camembert(110, 110, 100);
-          camembert.ajouterSecteur("red", 0.15);
-          camembert.ajouterSecteur("blue", 0.2);
-          camembert.ajouterSecteur("green", 0.65);
-          System.out.println(camembert.description(0));
-          camembert.createSvgFile();
-         
+
+        Camembert camembert = new Camembert(110, 110, 100);
+        camembert.ajouterSecteur("red", 0.15);
+        camembert.ajouterSecteur("blue", 0.2);
+        camembert.ajouterSecteur("green", 0.65);
+        System.out.println(camembert.description(0));
+        camembert.createSvgFile();
+
+        // Créez un ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Spécifiez le chemin vers votre fichier JSON
+        String cheminFichier = "ressources/prix-carburants-fichier-quotidien-test-ods.json";
+
+        // Essayez d'ouvrir et de mapper le fichier JSON en un objet Java
+        try {
+            // Utilisez la méthode readValue() de l'ObjectMapper pour mapper le fichier JSON
+            // en un objet Java.
+            List<StationParCarb> stations = objectMapper.readValue(new File(cheminFichier),
+                    new TypeReference<List<StationParCarb>>() {
+                    });
+
+            // Faites ce que vous voulez avec votre objet ici
+            HashMap<String, Station> stations2 = new HashMap<>();
+            for (StationParCarb station : stations) {
+                if (!stations2.containsKey(station.getId())) {
+                    stations2.put(station.getId(),
+                            new Station(station.getServices_service(), station.getDep_name(), station.getVille(),
+                                    station.getAdresse(),
+                                    station.getReg_name(), station.getDep_code(), station.getReg_code()));
+                }
+
+                stations2.get(station.getId()).ajoutCarburant(station.getPrix_nom(), station.getPrix_valeur());
+            }
+
+        } catch (IOException e) {
+            // Gérez les erreurs d'entrée/sortie ici
+            e.printStackTrace();
+        }
 
         // Autre manière de faire le camembert
         /*
