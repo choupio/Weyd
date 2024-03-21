7194be2d1c4bdbe207495396a4cebab854e47755
@apaillier <alan.paillier@etudiant.univ-rennes1.fr>
2024-03-21 16:43:48 +0100

    onglets

--- src/fr/univrennes/istic/l2gen/Interface/Onglet.java
+++ src/fr/univrennes/istic/l2gen/Interface/Onglet.java
@@ -1,5 +1,7 @@
 package fr.univrennes.istic.l2gen.Interface;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
@@ -10,12 +12,22 @@ public class Onglet {
 
     public Onglet(String titreOnglet1,String titreOnglet2,int WIDTH,int HEIGTH){
         onglets = new JTabbedPane(SwingConstants.TOP);
+
+        JButton button = new JButton("Rapport");
+
+
+        String[] Granularite={"Région","Departement"};
+        JComboBox<String> choixGranuralite = new JComboBox<>(Granularite);
+
+
         JPanel onglet1 = new JPanel();
         onglet1.setPreferredSize(new Dimension(WIDTH, HEIGTH));
         onglets.addTab(titreOnglet1, onglet1);
 
         JPanel onglet2 = new JPanel();
         onglet2.setPreferredSize(new Dimension(WIDTH,HEIGTH));
+        onglet2.add(choixGranuralite);
+        onglet2.add(button);
         onglets.addTab(titreOnglet2, onglet2);
         onglets.setOpaque(true);
     }
