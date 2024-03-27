f78eb1530e2980f389a78e6db26235b75c203c21
Sparenberg Maxance <maxance.sparenberg@etudiant.univ-rennes1.fr>
2024-03-27 18:25:44 +0100

    constructeur

--- src/fr/univrennes/istic/l2gen/station/StationAPI.java
+++ src/fr/univrennes/istic/l2gen/station/StationAPI.java
@@ -1,5 +1,47 @@
 package fr.univrennes.istic.l2gen.station;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class StationAPI {
+    private HashMap<String, Station> stationsMap;
+
+    public StationAPI() {
+        // Créez un ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Spécifiez le chemin vers votre fichier JSON
+        String cheminFichier = "ressources/prix-carburants-fichier-quotidien-test-ods.json";
+
+        // Essayez d'ouvrir et de mapper le fichier JSON en un objet Java
+        try {
+            // Utilisez la méthode readValue() de l'ObjectMapper pour mapper le fichier JSON
+            // en un objet Java.
+            List<StationParCarb> stations = objectMapper.readValue(new File(cheminFichier),
+                    new TypeReference<List<StationParCarb>>() {
+                    });
+
+            stationsMap = new HashMap<>();
+            for (StationParCarb station : stations) {
+                if (!stationsMap.containsKey(station.getId())) {
+                    stationsMap.put(station.getId(),
+                            new Station(station.getServices_service(), station.getDep_name(), station.getVille(),
+                                    station.getAdresse(),
+                                    station.getReg_name(), station.getDep_code(), station.getReg_code()));
+                }
+
+                stationsMap.get(station.getId()).ajoutCarburant(station.getPrix_nom(), station.getPrix_valeur());
+            }
+
+        } catch (IOException e) {
+            // Gérez les erreurs d'entrée/sortie ici
+            e.printStackTrace();
+        }
+    }
 
 }
