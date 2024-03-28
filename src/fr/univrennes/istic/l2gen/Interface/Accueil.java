489b479efc250ec9fc6d1670e73d18fb41e2fbb7
Juliette Karman-Besson <juliette.karman-besson@etudiant.univ-rennes.fr>
2024-03-28 15:26:28 +0100

    Modif interface

--- src/fr/univrennes/istic/l2gen/Interface/Accueil.java
+++ src/fr/univrennes/istic/l2gen/Interface/Accueil.java
@@ -8,18 +8,17 @@ public class Accueil {
     public Accueil() {
         ///////// Création de la fenêtre /////////
         CreationFenetre fenetre = new CreationFenetre("LES COURS DU CARBURANT EN FRANCE");
-        fenetre.ajouterImage("ressources/ImagesInterface/ImageTest.png");
+        // fenetre.ajouterImage("ressources/ImagesInterface/ImageTest.png");
 
         ///////// Taille de la fenêtre /////////
         // On récupère la taille de l'écran
         GraphicsEnvironment tailleEcran = GraphicsEnvironment.getLocalGraphicsEnvironment();
         // On créer le rectangle de la bonne taille
         Rectangle tailleUtilisable = tailleEcran.getMaximumWindowBounds();
-        // Onglet onglets = new Onglet("Acceuil", "Stat", (int)
-        // tailleUtilisable.getWidth(),
-        // (int) tailleUtilisable.getHeight());
-        // fenetre.add(onglets.GetPanel());
-        fenetre.setSize((int) tailleUtilisable.getWidth(), (int) tailleUtilisable.getHeight());
+        Onglet onglets = new Onglet("Acceuil", "Stat", (int) tailleUtilisable.getWidth(),
+                (int) tailleUtilisable.getHeight());
+        fenetre.getFenetre().add(onglets.GetPanel());
+        fenetre.getFenetre().setSize((int) tailleUtilisable.getWidth(), (int) tailleUtilisable.getHeight());
 
     }
 }
