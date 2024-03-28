298dd465b0b92ea03efd5c6d92e5cabc065a0f30
Juliette Karman-Besson <juliette.karman-besson@etudiant.univ-rennes.fr>
2024-03-28 13:40:22 +0100

    c'est rien

--- src/fr/univrennes/istic/l2gen/Interface/lancementTest.java
+++ src/fr/univrennes/istic/l2gen/Interface/lancementTest.java
@@ -1,8 +1,6 @@
 package fr.univrennes.istic.l2gen.Interface;
 
-import fr.univrennes.istic.l2gen.geometrie.Rectangle;
-
-public class lancementTest {
+public class LancementTest {
 
     public static void main(String[] args) throws Exception {
         Accueil test = new Accueil();
