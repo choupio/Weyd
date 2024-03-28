2a4e2b06183569c07c5833656e112001de51a39d
Juliette Karman-Besson <juliette.karman-besson@etudiant.univ-rennes.fr>
2024-03-28 16:43:02 +0100

    Modif accès JSON de DEPT et REG

--- src/fr/univrennes/istic/l2gen/Interface/Region.java
+++ src/fr/univrennes/istic/l2gen/Interface/Region.java
@@ -1,13 +1,10 @@
 package fr.univrennes.istic.l2gen.Interface;
 
 import java.awt.event.ItemEvent;
-import java.awt.*;
 import java.util.ArrayList;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.Border;
-
-import fr.univrennes.istic.l2gen.station.StationAPI;
 
 public class Region {
     private JPanel region = new JPanel();
@@ -16,19 +13,16 @@ public class Region {
         Border border = BorderFactory.createTitledBorder("région");
         this.region.setBorder(border);
 
-        StationAPI recup = new StationAPI();
+        ArrayList<String> tabRegion = Accueil.getRecup().getNomsRegion();
 
-        ArrayList<String> tabRegion = recup.getNomsRegion();
-
-    
         JCheckBox[] Checkbox = new JCheckBox[tabRegion.size()];
 
         for (int i = 0; i < tabRegion.size(); i += 1) {
             Checkbox[i] = new JCheckBox(tabRegion.get(i));
             this.region.add(Checkbox[i]);
-            int index=i;
-            Checkbox[i].addItemListener(new ItemListener(){
-                public void itemStateChanged(ItemEvent e){
+            int index = i;
+            Checkbox[i].addItemListener(new ItemListener() {
+                public void itemStateChanged(ItemEvent e) {
                     if (e.getStateChange() == ItemEvent.SELECTED) {
                         System.out.println("CheckBox" + index + "checked");
                     } else if (e.getStateChange() == ItemEvent.DESELECTED) {
