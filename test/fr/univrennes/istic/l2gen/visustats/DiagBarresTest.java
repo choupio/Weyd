282f5324635f3298583801902949bab913091856
ankouame <ange.kouame@etudiant.univ-rennes1.fr>
2024-03-29 21:07:10 +0100

    test

--- test/fr/univrennes/istic/l2gen/visustats/DiagBarresTest.java
+++ test/fr/univrennes/istic/l2gen/visustats/DiagBarresTest.java
@@ -1,21 +1,31 @@
 package fr.univrennes.istic.l2gen.visustats;
 
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DiagBarresTest {
+
+    DiagBarres diagBarres;
+
+    @Before
+    public void setUp() {
+        diagBarres = new DiagBarres("Test");
+    }
+
     @Test
     public void testAgencer() {
-
     }
 
     @Test
     public void testAjouterDonnees() {
-
+        diagBarres.ajouterDonnees("Data 1", 10.0, 20.0, 30.0);
+        diagBarres.ajouterDonnees("Data 2", 15.0, 25.0, 35.0);
+        assertNotNull(diagBarres.donnees);
     }
 
     @Test
     public void testAligner() {
-
     }
 
     @Test
@@ -25,22 +35,20 @@ public class DiagBarresTest {
 
     @Test
     public void testColorier() {
-
+        diagBarres.colorier("red", "blue", "green");
+        assertNotNull(diagBarres.couleurs);
     }
 
     @Test
     public void testCreateSvgFile() {
-
     }
 
     @Test
     public void testDeplacer() {
-
     }
 
     @Test
     public void testDescription() {
-
     }
 
     @Test
@@ -60,12 +68,10 @@ public class DiagBarresTest {
 
     @Test
     public void testLargeur() {
-
     }
 
     @Test
     public void testLegender() {
-
     }
 
     @Test
