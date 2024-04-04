3c93eb3c1b1123e1e4f744a5f115dfb1e244399d
ange kouame <ankouame@d024m05.istic.univ-rennes1.fr>
2024-04-04 17:06:14 +0200

    on avance surement mais doucement

--- src/fr/univrennes/istic/l2gen/rapport/Fonction.java
+++ src/fr/univrennes/istic/l2gen/rapport/Fonction.java
@@ -1,5 +1,3 @@
-
-
 package fr.univrennes.istic.l2gen.rapport;
 
 import java.io.BufferedWriter;
@@ -8,29 +6,24 @@ import java.io.IOException;
 
 public class Fonction {
     /**
-     * Crée un fichier HTML contenant le contenu spécifié.
+     * Crée un fichier HTML contenant le contenu HTML et SVG spécifiés.
      * 
-     * @param content    Le contenu HTML à inclure dans le fichier.
-     * @param htmlTitle  Le titre de la page HTML.
-     * @param fileName   Le nom du fichier HTML à créer.
+     * @param htmlContent   Le contenu HTML à inclure dans le fichier.
+     * @param svgContent    Le contenu SVG à inclure dans le fichier.
+     * @param htmlTitle     Le titre de la page HTML.
+     * @param fileName      Le nom du fichier HTML à créer.
      */
-    public static void createHTMLFile(String content, String htmlTitle, String fileName) {
-        String htmlContent = "<!DOCTYPE html>\n" + "<html lang=\"en\">\n" + "<head>\n" + "<meta charset=\"UTF-8\">\n"
-                + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + "<title>"
-                + htmlTitle + "</title>\n" + "</head>\n" + "<body>\n" + content + "\n" + "</body>\n" + "</html>";
+    public static void createHTMLFile(String htmlContent, String svgContent, String htmlTitle, String fileName) {
+        String htmlTemplate = "<!DOCTYPE html>\n" + "<html lang=\"en\">\n" + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n" + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "<title>" + htmlTitle + "</title>\n" + "</head>\n" + "<body>\n" + htmlContent + "\n" + svgContent + "\n"
+                + "</body>\n" + "</html>";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + ".html"))) {
-            writer.write(htmlContent);
+            writer.write(htmlTemplate);
             System.out.println("Fichier HTML créé avec succès !");
         } catch (IOException e) {
             System.err.println("Erreur lors de la création du fichier HTML : " + e.getMessage());
         }
     }
 }
-
-
-
-
-
-
-
