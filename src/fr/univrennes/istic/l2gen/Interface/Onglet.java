352b0b96bb3f8a1db6a4a79481c13e210484ed70
@apaillier <alan.paillier@etudiant.univ-rennes1.fr>
2024-04-04 18:55:29 +0200

    ajout checkBox + modification de la présentation graphique de l'onglet2

--- src/fr/univrennes/istic/l2gen/Interface/Onglet.java
+++ src/fr/univrennes/istic/l2gen/Interface/Onglet.java
@@ -21,6 +21,7 @@ public class Onglet {
         JPanel onglet2 = new JPanel();
         onglet2.setLayout(new BorderLayout());
         onglet2.setPreferredSize(new Dimension(WIDTH, HEIGTH));
+        //onglet2.setLayout(new BoxLayout(onglet2,BoxLayout.X_AXIS));
         this.onglets.addTab(titreOnglet2, onglet2);
 
         // Création d'un sous panel pour l'onglet 2
@@ -42,62 +43,82 @@ public class Onglet {
         Carburant carb = new Carburant();
         JPanel carburant = carb.Getcarburant();
         carburant.setLayout(new BoxLayout(carburant, BoxLayout.X_AXIS));
+
         // Création d'un JScrollPane pour dept
         JScrollPane scrollPaneDept = new JScrollPane(dept);
         scrollPaneDept.getVerticalScrollBar().setUnitIncrement(16); // Défilement plus rapide
         scrollPaneDept.setMaximumSize(new Dimension(230, HEIGTH - 500));
         scrollPaneDept.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
+        //création d'un JscrollPane pour serv
+        JScrollPane scrollPaneServ =new JScrollPane(serv);
+        scrollPaneServ.getVerticalScrollBar().setUnitIncrement(18);
+        scrollPaneServ.setMaximumSize(new Dimension(700, HEIGTH - 500));
+        scrollPaneServ.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+
+
         ////// Conteneurs pour mieux gérer la position à l'affichage //////
         // Création d'un JPanel pour contenir scrollPaneDept
         JPanel scrollPaneDeptContainer = new JPanel();
         scrollPaneDeptContainer.setLayout(new BoxLayout(scrollPaneDeptContainer, BoxLayout.X_AXIS));
         scrollPaneDeptContainer.add(scrollPaneDept);
+
         // Création d'un JPanel pour contenir region
         JPanel regionContainer = new JPanel();
         regionContainer.setLayout(new BoxLayout(regionContainer, BoxLayout.Y_AXIS));
         regionContainer.add(region);
+
         // Création d'un JPanel pour contenir carburant
         JPanel carburantContainer = new JPanel();
         carburantContainer.setLayout(new BoxLayout(carburantContainer, BoxLayout.X_AXIS));
         carburantContainer.add(carburant);
+
         // Création d'un JPanel pour contenir onglet21
         JPanel onglet21Container = new JPanel();
         onglet21Container.setLayout(new BoxLayout(onglet21Container, BoxLayout.Y_AXIS));
         onglet21Container.add(onglet21);
-        onglet21Container.add(Box.createVerticalStrut(20));
+        JCheckBox afficheStation=new JCheckBox("Afficher position des stations les moins chères");
+        afficheStation.setEnabled(false);
+        onglet21Container.add(afficheStation);
+        onglet21Container.add(Box.createVerticalStrut(10));
         onglet21Container.add(carburantContainer);
+
         // Création d'un JPanel pour contenir les stats
         JPanel statContainer = new JPanel();
         statContainer.setLayout(new BoxLayout(statContainer, BoxLayout.Y_AXIS));
         statContainer.add(stat);
+
         // Création d'un JPanel pour contenir les diag
         JPanel diagContainer = new JPanel();
         diagContainer.setLayout(new BoxLayout(diagContainer, BoxLayout.Y_AXIS));
         diagContainer.add(diag);
+
         // Création d'un panel pour contenir les diag et stats
         JPanel southContainer = new JPanel();
         southContainer.setLayout(new BoxLayout(southContainer, BoxLayout.X_AXIS));
         southContainer.add(statContainer, BorderLayout.WEST);
-        southContainer.add(Box.createHorizontalStrut(WIDTH - 850));
+        southContainer.add(onglet21Container,BorderLayout.CENTER);
         southContainer.add(diagContainer, BorderLayout.EAST);
-        onglet21Container.add(southContainer, BorderLayout.SOUTH);
+        
+
         // Création d'un JPanel pour contenir les services
         JPanel servContainer = new JPanel();
-        servContainer.setLayout(new BoxLayout(servContainer, BoxLayout.Y_AXIS));
-        servContainer.add(serv);
-
+        servContainer.setLayout(new BoxLayout(servContainer, BoxLayout.X_AXIS));
+        servContainer.add(scrollPaneServ);
+        
         // Ajout de ces panel à l'onglet 2
-        onglet2.add(onglet21Container, BorderLayout.NORTH);
+        onglet2.add(southContainer, BorderLayout.NORTH);
         onglet2.add(regionContainer, BorderLayout.WEST);
-        onglet2.add(scrollPaneDeptContainer, BorderLayout.EAST);
-        onglet2.add(servContainer, BorderLayout.CENTER);
+        onglet2.add(scrollPaneDeptContainer,BorderLayout.CENTER);
+        onglet2.add(servContainer, BorderLayout.EAST);
 
         // Gestion de la vision au démarrage
         region.setVisible(true);
         scrollPaneDeptContainer.setVisible(false);
         carburantContainer.setVisible(true);
         southContainer.setVisible(true);
+        servContainer.setVisible(true);
 
         // Création des éléments de contrôle pour l'onglet 2
         String[] Granularite = { "Régions", "Départements" };
