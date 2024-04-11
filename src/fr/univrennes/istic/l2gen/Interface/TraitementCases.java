d0febb6fc01cf468ed975740a6786d635e4dd002
cduchene <nathanael.brelaud@etudiant.univ-rennes.fr>
2024-04-11 16:27:22 +0200

    traitement de case

--- src/fr/univrennes/istic/l2gen/Interface/TraitementCases.java
+++ src/fr/univrennes/istic/l2gen/Interface/TraitementCases.java
@@ -11,20 +11,7 @@ public class TraitementCases {
     private Boolean isCheckedPos = Onglet.getIsSationsAffichees();
     private HashMap<String, Boolean> isCheckedReg = Region.getIsCheckedReg();
      
-    public void isCheckedDept(){
-        HashMap<String, Boolean> isCheckedDept = Departement.getIsCheckedDept();
-        for (String department : isCheckedDept.keySet()) {
-            Boolean isChecked = isCheckedDept.get(department);
-            System.out.println("Department: " + department + ", isChecked: " + isChecked);
-        }
-    }
-    public void isCheckedServ(){
-        HashMap<String, Boolean> isCheckedServ = Services.getIsCheckedServ();
-        for (String services : isCheckedServ.keySet()) {
-            Boolean isChecked = isCheckedServ.get(services);
-            System.out.println("Services: " + services + ", isChecked: " + isChecked);
-        }
-    }
+   
 
 
 }
