13b3e1cf6eaa39ff605d9ae0fb3f80529c9f94c3
cduchene <nathanael.brelaud@etudiant.univ-rennes.fr>
2024-04-18 16:20:53 +0200

    contrat

--- src/fr/univrennes/istic/l2gen/Interface/CreationFenetre.java
+++ src/fr/univrennes/istic/l2gen/Interface/CreationFenetre.java
@@ -5,9 +5,24 @@ import java.awt.Image;
 
 import javax.swing.*;
 
+/**
+ * Cette classe représente un créateur de fenêtre pour l'interface utilisateur.
+ * Elle permet de créer une fenêtre avec un titre spécifié et d'y ajouter une image.
+ */
 public class CreationFenetre {
+
+    /**
+     * Fenêtre créée par le créateur de fenêtre.
+     */
     private JFrame fenetre;
 
+    /**
+     * Constructeur de la classe CreationFenetre.
+     * Ce constructeur crée une fenêtre avec le titre spécifié et la rend visible.
+     * La fenêtre se ferme lorsque l'utilisateur la ferme.
+     *
+     * @param nomFenetre Le titre de la fenêtre.
+     */
     public CreationFenetre(String nomFenetre) {
         JFrame frame = new JFrame(nomFenetre);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,17 +30,22 @@ public class CreationFenetre {
         this.fenetre = frame;
     }
 
+    /**
+     * Méthode pour récupérer la fenêtre créée.
+     *
+     * @return La fenêtre créée.
+     */
     public JFrame getFenetre() {
         return fenetre;
     }
 
     /**
-     * Ajoute une image à la fenêtre à l'emplacement spécifié avec la taille
-     * spécifiée.
-     * 
-     * @param cheminImage chemin de l'image
-     * @param width       largeur de l'image
-     * @param height      hauteur de l'image
+     * Ajoute une image à un composant spécifié avec une taille spécifiée.
+     *
+     * @param composant   Le composant auquel ajouter l'image.
+     * @param cheminImage Le chemin de l'image à ajouter.
+     * @param width       La largeur de l'image.
+     * @param height      La hauteur de l'image.
      */
     public void ajouterImage(JComponent composant, String cheminImage, int width, int height) {
 
