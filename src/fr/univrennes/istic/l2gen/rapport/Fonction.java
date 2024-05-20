a222ba60d6007ee309d38ad36cd402f8c36fd66f
ange kouame <ange.kouame@etudiant.univ-rennes1.fr>
2024-05-20 22:38:37 +0200

    modification de la taille pour une meilleure visualisation

--- src/fr/univrennes/istic/l2gen/rapport/Fonction.java
+++ src/fr/univrennes/istic/l2gen/rapport/Fonction.java
@@ -33,7 +33,7 @@ public class Fonction {
                 "  svg {\n" +
                 "    display: block;\n" +
                 "    margin: 0;\n" +
-                "    width:  25%;\n" +
+                "    width:  80%;\n" +
                 "    background-color: white; \n" +
                 "  }\n" +
                 "  h2 {\n" +
